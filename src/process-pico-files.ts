eb3d444016e20515e7623b90949c96a844729fa0
Gregory Denys <gregory.g.denys@gmail.com>
2022-06-08 19:50:47 -0400

    update linting configuration

--- src/process-pico-files.ts
+++ src/process-pico-files.ts
@@ -11,76 +11,76 @@ const logReadFileAction = (file: string) => process.stdout.write(`Reading file: 
 const logWriteFileAction = (file: string) => process.stdout.write(`Writing file: ${file}`)
 
 function handleError(error: Error, message: string) {
-	if (error) {
-		process.stderr.write(`Error while ${message} with error: ${error}`)
-	}
+    if(error) 
+        process.stderr.write(`Error while ${message} with error: ${error}`)
+	
 }
 
 function createScopedPicoFile(
-	originalFileName: string, 
-	originalCss: string,
-	outputDirectory: string
+    originalFileName: string, 
+    originalCss: string,
+    outputDirectory: string
 ): Promise<void> {
-	const picoStylesGlobalScopeName = '.pico-styles'
-	const rootCssPseudoClassRegex = /:root/g
+    const picoStylesGlobalScopeName = '.pico-styles'
+    const rootCssPseudoClassRegex = /:root/g
 
-	return postcss([postcssPrefixWrap('.pico-styles')])
-		.process(originalCss, { from: undefined })
-		.then((result: { css: string }) => {
-			const processedCss = result.css.replace(rootCssPseudoClassRegex, picoStylesGlobalScopeName)
-			const processedCssFileName = `scoped.${originalFileName}`
-			const processedFileAndDirectory = `${outputDirectory}/${processedCssFileName}`
+    return postcss([postcssPrefixWrap('.pico-styles')])
+        .process(originalCss, { from: undefined })
+        .then((result: { css: string }) => {
+            const processedCss = result.css.replace(rootCssPseudoClassRegex, picoStylesGlobalScopeName)
+            const processedCssFileName = `scoped.${originalFileName}`
+            const processedFileAndDirectory = `${outputDirectory}/${processedCssFileName}`
 
-			logWriteFileAction(processedFileAndDirectory)
+            logWriteFileAction(processedFileAndDirectory)
 
-			if (!fs.existsSync(outputDirectory)) {
-				fs.mkdirSync(outputDirectory, { recursive: true });
-			}
+            if(!fs.existsSync(outputDirectory)) 
+                fs.mkdirSync(outputDirectory, { recursive: true })
+			
 
-			fs.writeFileSync(
-				processedFileAndDirectory,
-				processedCss,
-				handleWriteFileError
-			)
-		})
-		.catch(handleProcessingFileError)
+            fs.writeFileSync(
+                processedFileAndDirectory,
+                processedCss,
+                handleWriteFileError
+            )
+        })
+        .catch(handleProcessingFileError)
 }
 
 function createScopedPicoFiles(
-	inputDirectory: string,
-	outputDirectory: string,	
+    inputDirectory: string,
+    outputDirectory: string,	
 ): Array<Promise<void>> {
-	const originalPicoFiles = [...fs.readdirSync(inputDirectory, handleReadFilesError)]
-	const promises: Array<Promise<void>> = originalPicoFiles.map((originalFileName) => {
-		const originalFileAndDirectory = `${inputDirectory}/${originalFileName}`
+    const originalPicoFiles = [...fs.readdirSync(inputDirectory, handleReadFilesError)]
+    const promises: Array<Promise<void>> = originalPicoFiles.map((originalFileName) => {
+        const originalFileAndDirectory = `${inputDirectory}/${originalFileName}`
 
-		logReadFileAction(originalFileAndDirectory)
+        logReadFileAction(originalFileAndDirectory)
 		
-		return new Promise((resolve) => {
-			fs.readFile(`${inputDirectory}/${originalFileName}`, async (readFileError: Error, originalCss: string) => {
-				handleReadFileError(readFileError)
+        return new Promise((resolve) => {
+            fs.readFile(`${inputDirectory}/${originalFileName}`, async (readFileError: Error, originalCss: string) => {
+                handleReadFileError(readFileError)
 
-				await createScopedPicoFile(originalFileName, originalCss, outputDirectory)
+                await createScopedPicoFile(originalFileName, originalCss, outputDirectory)
 
-				resolve()
-			})
-		}) 
-	})
+                resolve()
+            })
+        }) 
+    })
 
-	return promises
+    return promises
 }
 
 function processFiles(): Array<Promise<void>>  {
-	const originalCssFilesDirectory = './pico-original-files'
-	const processedCssFilesDirectory = './pico-processed-files'
+    const originalCssFilesDirectory = './pico-original-files'
+    const processedCssFilesDirectory = './pico-processed-files'
 
-	return createScopedPicoFiles(originalCssFilesDirectory, processedCssFilesDirectory)
+    return createScopedPicoFiles(originalCssFilesDirectory, processedCssFilesDirectory)
 }
 
 export {
-	createScopedPicoFile,
-	createScopedPicoFiles,
-	processFiles
+    createScopedPicoFile,
+    createScopedPicoFiles,
+    processFiles
 }
 
 
--- test/process-pico-files.test.ts
+++ test/process-pico-files.test.ts
@@ -1,6 +1,6 @@
 
+const { createScopedPicoFiles } = require('../src/process-pico-files')
 const fs = require("fs")
-import { createScopedPicoFiles } from '../src/process-pico-files'
 
 const mockInputCssFileName = 'original-file-1.css'
 const mockOutputCssFileName = 'scoped.' + mockInputCssFileName
@@ -10,20 +10,20 @@ const expectedOutputCssDirectory = `${process.cwd()}/test`
 const expectedOutputCssFileName = 'expected-output.css'
 
 describe('createScopedPicoFiles', () => {
-	beforeEach(() => {
-		if (fs.existsSync(mockOutputDirectory)) {
-			fs.rmdirSync(mockOutputDirectory, { recursive: true });
-		}
+    beforeEach(() => {
+        if(fs.existsSync(mockOutputDirectory)) 
+            fs.rmdirSync(mockOutputDirectory, { recursive: true })
+		
     })
 
-	test('should correctly process pico css files', () => {
+    test('should correctly process pico css files', () => {
 	
-		return Promise.all(createScopedPicoFiles(mockInputDirectory, mockOutputDirectory)).then(() => {
-			const actualOutputCss = fs.readFileSync(`${mockOutputDirectory}/${mockOutputCssFileName}`, 'utf8')
-			const expectedOutputCss = fs.readFileSync(`${expectedOutputCssDirectory}/${expectedOutputCssFileName}`, 'utf8')
+        return Promise.all(createScopedPicoFiles(mockInputDirectory, mockOutputDirectory)).then(() => {
+            const actualOutputCss = fs.readFileSync(`${mockOutputDirectory}/${mockOutputCssFileName}`, 'utf8')
+            const expectedOutputCss = fs.readFileSync(`${expectedOutputCssDirectory}/${expectedOutputCssFileName}`, 'utf8')
 
-			expect(actualOutputCss).toEqual(expectedOutputCss)	
-		})
-	})
+            expect(actualOutputCss).toEqual(expectedOutputCss)	
+        })
+    })
 })	
 
