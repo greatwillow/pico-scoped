7f95485a07df936df5dffe8f649e932c21c96c42
Gregory Denys <gregory.g.denys@gmail.com>
2022-06-08 19:00:22 -0400

    add process files tests

--- src/process-pico-files.ts
+++ src/process-pico-files.ts
@@ -2,36 +2,32 @@ const postcss = require('postcss')
 const fs = require('fs')
 const postcssPrefixWrap = require('postcss-prefixwrap')
 
-const orignalCssFilesDirectory = './pico-original-files'
-const processedCssFilesDirectory = './pico-processed-files'
-const picoStylesGlobalScopeName = '.pico-styles'
-const rootCssPseudoClassRegex = /:root/g
-
 const handleReadFilesError = (error: Error) => handleError(error, 'reading files')
 const handleReadFileError = (error: Error) => handleError(error, 'reading file')
 const handleWriteFileError = (error: Error) => handleError(error, 'writing file')
 const handleProcessingFileError = (error: Error) => handleError(error, 'processing file')
 
-const logReadFileAction = (file: string) => console.log('Reading file: ', file)
-const logWriteFileAction = (file: string) => console.log('Writing file: ', file)
+const logReadFileAction = (file: string) => process.stdout.write(`Reading file: ${file}`)
+const logWriteFileAction = (file: string) => process.stdout.write(`Writing file: ${file}`)
 
 function handleError(error: Error, message: string) {
 	if (error) {
-		console.error(`Error while ${message} with error: ${error}`)
+		process.stderr.write(`Error while ${message} with error: ${error}`)
 	}
 }
 
 function createScopedPicoFile(
 	originalFileName: string, 
 	originalCss: string,
-	outputDirectory: string,
-	cssGlobalScopeName: string = picoStylesGlobalScopeName,
-	rootRegex: RegExp = rootCssPseudoClassRegex
-) {
-	postcss([postcssPrefixWrap(cssGlobalScopeName)])
+	outputDirectory: string
+): Promise<void> {
+	const picoStylesGlobalScopeName = '.pico-styles'
+	const rootCssPseudoClassRegex = /:root/g
+
+	return postcss([postcssPrefixWrap('.pico-styles')])
 		.process(originalCss, { from: undefined })
 		.then((result: { css: string }) => {
-			const processedCss = result.css.replace(rootRegex, cssGlobalScopeName)
+			const processedCss = result.css.replace(rootCssPseudoClassRegex, picoStylesGlobalScopeName)
 			const processedCssFileName = `scoped.${originalFileName}`
 			const processedFileAndDirectory = `${outputDirectory}/${processedCssFileName}`
 
@@ -41,7 +37,7 @@ function createScopedPicoFile(
 				fs.mkdirSync(outputDirectory, { recursive: true });
 			}
 
-			fs.writeFile(
+			fs.writeFileSync(
 				processedFileAndDirectory,
 				processedCss,
 				handleWriteFileError
@@ -53,26 +49,38 @@ function createScopedPicoFile(
 function createScopedPicoFiles(
 	inputDirectory: string,
 	outputDirectory: string,	
-	) {
+): Array<Promise<void>> {
 	const originalPicoFiles = [...fs.readdirSync(inputDirectory, handleReadFilesError)]
-
-	originalPicoFiles.forEach((originalFileName) => {
+	const promises: Array<Promise<void>> = originalPicoFiles.map((originalFileName) => {
 		const originalFileAndDirectory = `${inputDirectory}/${originalFileName}`
 
 		logReadFileAction(originalFileAndDirectory)
+		
+		return new Promise((resolve) => {
+			fs.readFile(`${inputDirectory}/${originalFileName}`, async (readFileError: Error, originalCss: string) => {
+				handleReadFileError(readFileError)
 
-		fs.readFile(`${inputDirectory}/${originalFileName}`, (readFileError: Error, originalCss: string) => {
-			handleReadFileError(readFileError)
-			createScopedPicoFile(originalFileName, originalCss, outputDirectory)
-		})
+				await createScopedPicoFile(originalFileName, originalCss, outputDirectory)
+
+				resolve()
+			})
+		}) 
 	})
+
+	return promises
 }
 
-createScopedPicoFiles(orignalCssFilesDirectory, processedCssFilesDirectory)
+function processFiles(): Array<Promise<void>>  {
+	const originalCssFilesDirectory = './pico-original-files'
+	const processedCssFilesDirectory = './pico-processed-files'
+
+	return createScopedPicoFiles(originalCssFilesDirectory, processedCssFilesDirectory)
+}
 
 export {
 	createScopedPicoFile,
-	createScopedPicoFiles
+	createScopedPicoFiles,
+	processFiles
 }
 
 
---
+++ test/process-pico-files.test.ts
@@ -0,0 +1,29 @@
+
+const fs = require("fs")
+import { createScopedPicoFiles } from '../src/process-pico-files'
+
+const mockInputCssFileName = 'original-file-1.css'
+const mockOutputCssFileName = 'scoped.' + mockInputCssFileName
+const mockInputDirectory = `${process.cwd()}/test/test-input-directory`
+const mockOutputDirectory = `${process.cwd()}/test/test-output-directory`
+const expectedOutputCssDirectory = `${process.cwd()}/test`
+const expectedOutputCssFileName = 'expected-output.css'
+
+describe('createScopedPicoFiles', async () => {
+	beforeEach(() => {
+		if (fs.existsSync(mockOutputDirectory)) {
+			fs.rmdirSync(mockOutputDirectory, { recursive: true });
+		}
+    })
+
+	test('should correctly process pico css files', () => {
+	
+		return Promise.all(createScopedPicoFiles(mockInputDirectory, mockOutputDirectory)).then(() => {
+			const actualOutputCss = fs.readFileSync(`${mockOutputDirectory}/${mockOutputCssFileName}`, 'utf8')
+			const expectedOutputCss = fs.readFileSync(`${expectedOutputCssDirectory}/${expectedOutputCssFileName}`, 'utf8')
+
+			expect(actualOutputCss).toEqual(expectedOutputCss)	
+		})
+	})
+})	
+
